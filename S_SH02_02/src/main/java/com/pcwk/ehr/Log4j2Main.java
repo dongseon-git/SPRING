a8c4b84214333be5902b013518ea655cb8246c06
dongsun <seon970619@icloud.com>
2022-05-29 14:32:59 +0900

    init

---
+++ S_SH02_02/src/main/java/com/pcwk/ehr/Log4j2Main.java
@@ -0,0 +1,14 @@
+package com.pcwk.ehr;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2Main {
+final static Logger LOG = LogManager.getLogger(Log4j2Main.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		LOG.debug("=============");
+		LOG.debug("=Log4j2=");
+		LOG.debug("=============");
+	}
+
+}
